0bf899264e789421a9d0fabf11fc8f459e878c83
Gaëtan H <gaetanhus@gmail.com>
2026-03-20 18:49:02 +0100

    v0.5.0

--- src/cli/args.ts
+++ src/cli/args.ts
@@ -42,7 +42,7 @@ export const createProgram = (): Command => {
   program
     .name("mkicon")
     .description("Transform SVG icons into beautiful React, Vue, or Svelte components")
-    .version("0.4.1")
+    .version("0.5.0")
     .helpOption("-h, --help", "Display help");
 
   program
